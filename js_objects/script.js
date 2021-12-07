7abfbdf7193279ee96a52f8236635d4a6e72eab0
Василий Игнатков <sibimaqy@gmail.com>
2021-12-07 22:48:20 +0700

    Fix js objects

--- js_objects/script.js
+++ js_objects/script.js
@@ -6,46 +6,56 @@
                 {
                     name: "Rome",
                     population: 2873000
-                }, {
+                },
+                {
                     name: "Florence",
                     population: 382258
-                }, {
+                },
+                {
                     name: "Naples",
                     population: 3085000
-                }, {
+                },
+                {
                     name: "Milan",
                     population: 1352000
                 }
             ]
-        }, {
+        },
+        {
             name: "France",
             cities: [
                 {
                     name: "Paris",
                     population: 2161000
-                }, {
+                },
+                {
                     name: "Marseilles",
                     population: 861635
-                }, {
+                },
+                {
                     name: "Lion",
                     population: 513275
-                }, {
+                },
+                {
                     name: "Nantes",
                     population: 309346
                 }
             ]
-        }, {
+        },
+        {
             name: "United Kingdom",
             cities: [
                 {
                     name: "London",
                     population: 8982000
-                }, {
+                },
+                {
                     name: "Edinburgh",
                     population: 482005
                 }
             ]
-        }, {
+        },
+        {
             name: "Spain",
             cities: [
                 {
@@ -60,40 +70,27 @@
         console.log("Countries with max cities count " + country.name);
     });
 
-    console.log(getCountriesPopulation(countries));
+    console.log(getCountriesPopulations(countries));
 })();
 
 function getCountriesWithMaxCitiesCount(countries) {
-    var maxCitiesCount = 0;
-
-    if (countries.length !== 0) {
-        maxCitiesCount = countries[0].cities.length;
-    } else {
-        return [];
-    }
-
-    maxCitiesCount = countries.reduce(function (previous, current) {
-        return Math.max(maxCitiesCount, current.cities.length);
-    });
-
-    console.log("Max cities count = " + maxCitiesCount);
+    var maxCitiesCount = countries.reduce(function (accumulator, current) {
+        return Math.max(accumulator, current.cities.length);
+    }, 0);
 
     return countries.filter(function (country) {
         return country.cities.length === maxCitiesCount;
     });
 }
 
-function getCountriesPopulation(countries) {
-    var countryPopulation = {};
+function getCountriesPopulations(countries) {
+    var countriesPopulations = {};
 
     countries.forEach(function (country) {
-        var population = 0;
-        population = country.cities.reduce(function (accumulator, current) {
-            return accumulator + current.population;
-        }, population);
-
-        countryPopulation[country.name] = population;
+        countriesPopulations[country.name] = country.cities.reduce(function (countryPopulations, city) {
+            return countryPopulations + city.population;
+        }, 0);
     });
 
-    return countryPopulation;
+    return countriesPopulations;
 }
